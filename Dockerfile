2d2f9466930a41c220dea1227a56c491e2b8917c
Francesco Frassinelli <francesco.frassinelli@nina.no>
2023-02-09 18:03:08 +0100

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM debian:bookworm
+
+RUN apt-get update && \
+    apt-get install -yq --no-install-recommends \
+        wget xxd jpegoptim python3-pdm \
+        libgl1 libglib2.0 `# opencv`
+
+WORKDIR /root
+RUN wget -qO - https://github.com/frafra/BlurDetection2/archive/refs/heads/improved.tar.gz | \
+        tar xz --strip-components=1
+RUN pdm install --no-self --no-lock
+
+COPY --chmod=755 evaluering.sh .
+ENTRYPOINT ["./evaluering.sh"]
+CMD ["/data"]
---
+++ evaluering.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Bash configuration
+set -Eeuo pipefail
+shopt -s globstar nullglob
+
+# Global options
+DIR="$1"
+
+# Prepare data
+for file in "$DIR"/**/*.JPG
+do
+    jpegoptim --strip-all "$file"
+    new_path="$(dirname "$file")/$(xxd -p -l 16 /dev/random).JPG"
+    mv "$file" "$new_path"
+done
+
+# Compute values
+echo -e 'size\tlaplacian_variance\tpath'
+for file in "$DIR"/**/*.JPG
+do
+    {
+    du -b "$file" | awk '{ print $1 }'
+    pdm run python3 process.py `# -t 300` -i "$file" |&
+        sed -n 's/.*score: \([0-9]\+\.[0-9]\+\) .*/\1/p'
+    echo -n "$file"
+    } | tr '\n' $'\t'
+    echo
+done
